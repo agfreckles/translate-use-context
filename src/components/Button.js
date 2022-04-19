f2c13450ea6f5d377c44b7a445489d7009d948fc
agfreckles <oluueli@gmail.com>
2022-04-19 21:02:51 +0000

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+import LanguageContext from "./contexts/LanguageContext";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div>
+        <button className="ui button primary">
+          <LanguageContext.Consumer>
+            {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+          </LanguageContext.Consumer>
+        </button>
+      </div>
+    );
+  }
+}
+export default Button;
